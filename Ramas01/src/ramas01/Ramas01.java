2311e92dc26b6f21a03d974e82ff3529e475ecb9
PzDarby <izliquihd@gmail.com>
2024-11-05 11:32:27 +0100

    primer commit

---
+++ Ramas01/src/ramas01/Ramas01.java
@@ -0,0 +1,10 @@
+package ramas01;
+
+public class Ramas01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Ramas01/src/util/Funciones.java
@@ -0,0 +1,48 @@
+package util;
+
+import java.util.Scanner;
+
+public class Funciones {
+	public static boolean esInt(String num) {
+		try {
+			Integer.parseInt(num);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	public static boolean esDouble(String num) {
+		try {
+			Double.parseDouble(num);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	public static int dimeEntero(String texto ,Scanner sc) {
+		int num=0;
+		do {
+			try {
+				System.out.println(texto);
+				String s=sc.nextLine();
+				num =Integer.parseInt(s);
+				return num;
+			} catch (Exception e) {
+				System.out.println("Numero no valido");
+			}
+		}while(true);
+	}
+	public static double dimeDouble(String texto,Scanner sc) {
+		double num=0;
+		do {
+			try {
+				System.out.println(texto);
+				String s=sc.nextLine();
+				num =Double.parseDouble(s);
+				return num;
+			} catch (Exception e) {
+				System.out.println("Numero no valido");
+			}
+		}while(true);
+	}
+}
